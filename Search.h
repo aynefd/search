3b89fc26df4e79ccce6a50bd38bab52159396d1e
aynefd <aynedelgado@gmail.com>
2015-02-12 10:26:56 -0700

    Rename search.cpp to Search.h

--- Search.h
+++ Search.h
@@ -1,3 +1,9 @@
+//I Ayne Delgado have not used any code other than my own (or that in the textbook) for this project. 
+//I also have not used any function or data-structure from the Standard-Template Library.
+//I understand that any violation of this disclaimer will result in a 0 for the project.
+
+//-------------------------Search header file---------------------------------------
+
 #pragma once
 
 #include <iostream>
